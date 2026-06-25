afd7bf4b103a64bd97f9c49e761a08c45a7422bb
bhaskarxtrem <rishu93159838@gmail.com>
2026-06-25 23:46:31 +0530

    Starting my new OOP practice project

---
+++ library-book-manager.py
@@ -0,0 +1,13 @@
+books = []
+
+def menu():
+    print("1. Add book")
+    print("2. Show all books")
+    print("3. Change Book Title")
+    print("4. Change Book Author")
+    print("5. Change number of pages")
+    print("6. Exit")
+
+
+class book:
+    
