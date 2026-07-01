94a97e5b927a1a0ec051f4e1b27892fb4df8aba6
bhaskarxtrem <rishu93159838@gmail.com>
2026-07-01 22:50:48 +0530

    Library book manager v2 completed

--- library-book-manager.py
+++ library-book-manager.py
@@ -20,7 +20,10 @@ def add_member():
     member_name = input("Enter your name: ")
     member_age = input("Enter your age: ")
     member_gender = input("what's your gender[Male/Female]: ").lower()
-    member_id = id + 1
+    
+    id += 1
+
+    member_id = id
 
     new_member = Member(member_name, member_age, member_id, member_gender)
 
@@ -34,6 +37,11 @@ def all_books(books):
     for i in range(len(books)):
         print(f"\nBook {i+1}: ")
         books[i].show_books()
+
+def all_members(members):
+    for i in range(len(members)):
+        print(f"\nMember {i+1}: ")
+        members[i].show_member()
 
 def menu():
     print("---------------------------------")
@@ -51,7 +59,7 @@ def menu():
     print("---------------------------------")
     print("11. Exit")
 
-def operations(choice, books):
+def operations(choice, books, members):
     if choice == 1:
         add_book()
     elif choice == 2:
@@ -71,7 +79,7 @@ def operations(choice, books):
                     profile = int(input("Enter the book number you wanna change title of: "))
                     new_title = input("Enter the name you wanna upgrade: ")
 
-                    books[profile-1].change_title(new_title)
+                    books[profile - 1].change_title(new_title)
                     print("Your title is updated!")
                     input("Enter to continue...")
                     break
@@ -91,7 +99,7 @@ def operations(choice, books):
                     profile = int(input("Enter the book number you wanna change author of: "))
                     new_author = input("Enter the name you wanna upgrade: ")
 
-                    books[profile-1].change_author(new_author)
+                    books[profile - 1].change_author(new_author)
                     print("Your author is updated!")
                     input("Enter to continue...")
                     break
@@ -113,15 +121,87 @@ def operations(choice, books):
                     profile = int(input("Enter the book number you wanna change pages of: "))
                     new_pages = int(input("Enter the pages number you wanna update: "))
 
-                    books[profile-1].change_pages(new_pages)
+                    books[profile - 1].change_pages(new_pages)
                     print("Your pages is updated!")
                     input("Enter to continue...")
                     break
                 except:
                     print("Enter only book number to procced!")
         else:
-            print("No books found!")
+            print("No books found! ")
             input("Enter to continue! ")
+
+    elif choice == 6:
+        add_member()
+
+    elif choice == 7:
+        if members:
+            all_members(members)
+            input("Enter to continue! ")
+        else:
+            print("No members found! ")
+            input("Enter to continue! ")
+
+    elif choice == 8:
+        if members:
+            all_members(members)
+            print(f"\nTHESE ARE ALL THE MEMBERS OF LIBRARY, CHOOSE THE MEMBER NUMBER TO UPDATE NAME! ")
+
+            while True:
+                try:
+                    member = int(input("Enter the member number to change name: "))
+                    new_name = input("Enter the name you wanna update: ")
+                    members[member - 1].change_name(new_name)
+                    print("Your name is updated! ")
+                    input("Enter to continue! ")
+                    break
+                except:
+                    print("Enter only member number to procced! ")
+
+        else:
+            print("No members found! ")
+            input("Enter to continue! ")
+
+    elif choice == 9:
+        if members:
+            all_members(members)
+            print(f"\nTHESE ARE ALL THE MEMBERS OF LIBRARY, CHOOSE THE MEMBER NUMBER TO UPDATE AGE! ")
+
+            while True:
+                try:
+                    member = int(input("Enter the member number to change age: "))
+                    new_age = input("Enter the age you wanna update: ")
+                    members[member - 1].change_age(new_age)
+                    print("Your age is updated! ")
+                    input("Enter to continue! ")
+                    break
+                except:
+                    print("Enter only member number to procced! ")
+
+        else:
+            print("No members found! ")
+            input("Enter to continue! ")
+
+    elif choice == 10:
+        if members:
+            all_members(members)
+            print(f"\nTHESE ARE ALL THE MEMBERS OF LIBRARY, CHOOSE THE MEMBER NUMBER TO UPDATE GENDER! ")
+
+            while True:
+                try:
+                    member = int(input("Enter the member number to change Gender: "))
+                    new_gender = input("What Gender you wanna update [Male/Female]: ").lower()
+                    members[member - 1].change_gender(new_gender)
+                    print("Your Gender is updated! ")
+                    input("Enter to continue! ")
+                    break
+                except:
+                    print("Enter only member number to procced! ")
+
+        else:
+            print("No members found! ")
+            input("Enter to continue! ")
+
 
     elif choice == 11:
         exit = True
@@ -148,27 +228,27 @@ class Book:
         self.pages = pages
 
 
-    class Member:
-        def __init__(self, name, age, member_id, gender ):
-            self.name = name
-            self.age = age
-            self.member_id = member_id
-            self.gender = gender
+class Member:
+    def __init__(self, name, age, member_id, gender ):
+        self.name = name
+        self.age = age
+        self.member_id = member_id
+        self.gender = gender
 
-        def show_member(self):
-            print(f"Member name: {self.name}")
-            print(f"Member age: {self.age}")
-            print( f"Member ID: {self.member_id}")
-            print(f"gender: {self.gender}")
+    def show_member(self):
+        print(f"Member name: {self.name}")
+        print(f"Member age: {self.age}")
+        print( f"Member ID: {self.member_id}")
+        print(f"gender: {self.gender}")
 
-        def change_name(self, name):
-            self.name = name
+    def change_name(self, name):
+        self.name = name
 
-        def change_age(self, age):
-            self.age = age
-        
-        def change_gender(self, gender):
-            self.gender = gender
+    def change_age(self, age):
+        self.age = age
+    
+    def change_gender(self, gender):
+        self.gender = gender
 
 
 while True:
@@ -176,12 +256,12 @@ while True:
         try:
             menu()
             choice = int(input("\nEnter the operation number you wanna do: "))
-            exit = operations(choice, books)
+            exit = operations(choice, books, members)
             if exit:
                 break
             else:
                 continue
 
-        except:
-            print("Only type operation number, try again")
+        except Exception as e:
+            print(e)
     break
